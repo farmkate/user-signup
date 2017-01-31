e66276d8b861ea69bf5bee456cd618b4adf536b2
Kathryn Holden <Kathryn Holden>
2017-01-30 19:55:01 -0600

    form working except for escaping the username and email input

--- main.py
+++ main.py
@@ -38,9 +38,10 @@ def valid_password(password, verify_password):
     return PASS_RE.match(password) and password==verify_password and password
 
 def valid_email(email):
-    #if not EMAIL_RE.match(email):
-        #error_dictionary[EMAIL] = error_message('email')
-    return EMAIL_RE.match(email)
+    if email:
+        return EMAIL_RE.match(email)
+    else:
+        return True
 
 #error_dictionary = {'USERNAME': '', 'PASSWORD': '', 'EMAIL': ''}
 #username_error = "You didn't enter a valid username.  Try again."
@@ -76,25 +77,25 @@ class MainHandler(webapp2.RequestHandler):
         <form action = '/complete' method = 'post'>
             <label>
                 Username
-                <input type='text' name='username' />
-                <div style="color: red; display: inline-block">{0}</div>
+                <input type='text' name='username' value='{USERNAME_INPUT}'/>
+                <div style="color: red; display: inline-block">{USERNAME_ERROR}</div>
             </label>
             <br>
             <label>
                 Password
-                <input type='text' name='password' />
-                <div style="color: red; display: inline-block">{1}</div>
+                <input type='password' name='password' />
+                <div style="color: red; display: inline-block">{PASSWORD_ERROR}</div>
             </label>
             <br>
             <label>
                 Verify Password
-                <input type='text' name='verify_password' />
+                <input type='password' name='verify_password' />
             </label>
             <br>
             <label>
                 Email (optional)
-                <input type='text' name='email' />
-                <div style="color: red; display: inline-block">{2}</div>
+                <input type='text' name='email' value='{EMAIL_INPUT}'/>
+                <div style="color: red; display: inline-block">{EMAIL_ERROR}</div>
             </label>
             <br>
             <input type="submit" value="Submit Query">
@@ -105,41 +106,43 @@ class MainHandler(webapp2.RequestHandler):
         password_error = self.request.get("password_error")
         email_error = self.request.get("email_error")
 
-#        if username_error or password_error or email_error:
-#            error_esc = cgi.escape(error_list), quote=True)
-            #error_element = '<p class="error">' + error_esc + '</p>'
-#            self.response.write(page_header + form.format(error_esc)+ page_footer)
-#        else:
-#            error_esc =('', '', '')
-        self.response.write(page_header + form.format(username_error, password_error, email_error)+ page_footer)
+        username_input=self.request.get("username_input")
+        email_input=self.request.get("email_input")
 
-#        self.response.write(page_header + form.format(error_esc)+ page_footer)
+        #esc_username_input=cgi.escape(username_input, quote=True)
+        #esc_email_input=cgi.escape(email_input, quote=True)
+
+        self.response.write(page_header + form.format(USERNAME_INPUT=username_input, USERNAME_ERROR=username_error, PASSWORD_ERROR=password_error, EMAIL_INPUT=email_input, EMAIL_ERROR=email_error)+ page_footer)
+
 
     def get(self):
         self.write_form()
 
-#        main_content = form + '<br>' + error_element
-#        content = page_header + main_content +  page_footer
-#        self.response.write(page_header + (form.format(error_esc))+ page_footer)
 
 class SignedUp(webapp2.RequestHandler):
     """ Handles requests coming in to '/signup'
         e.g. www.user-signup.com/add
     """
     def post(self):
-        username = self.request.get('username')
+        username_input = self.request.get('username')
         password = self.request.get('password')
         verify_password = self.request.get('verify_password')
-        email = self.request.get('email')
+        email_input = self.request.get('email')
 
-        username_ok = valid_username(username)
+        username_ok = valid_username(username_input)
         password_ok = valid_password(password, verify_password)
-        email_ok = valid_email(email)
+        email_ok = valid_email(email_input)
+
+        #esc_username_input = ''
+        #esc_email_input = ''
 
         if username_ok:
             username_error_message = ''
+            esc_username_input = ''
         else:
             username_error_message = error_message('username')
+            esc_username_input = cgi.escape(username_input, quote=True)
+
 
         if password_ok:
             password_error_message = ''
@@ -148,13 +151,13 @@ class SignedUp(webapp2.RequestHandler):
 
         if email_ok:
             email_error_message = ''
+            esc_email_input = ''
         else:
             email_error_message = error_message('email')
-
-        #error_list = [usename_error_message, password_error_message, email_error_message]
+            esc_email_input = cgi.escape(email_input, quote=True)
 
         if not username_ok or not password_ok or not email_ok:
-            self.redirect('/?username_error=' + username_error_message + '&password_error=' + password_error_message + '&email_error=' + email_error_message)
+            self.redirect('/?username_error=' + username_error_message + '&username_input=' + esc_username_input + '&password_error=' + password_error_message + '&email_error=' + email_error_message + '&email_input=' + esc_email_input)
 
         congrats = 'Thanks for signing up!' + '<br>'
         self.response.write(congrats)
